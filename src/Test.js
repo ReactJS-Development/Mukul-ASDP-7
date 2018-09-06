ff39c9e36f03c22ef488efcfe8c58771d2fdeb5a
Mukulsharmahsr5 <mukulsharmahsr5@gmail.com>
2018-09-06 12:59:36 +0530

    new files added

---
+++ src/Test.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Test extends React.Component {
+	state = {
+		clickCounter: 0,
+		currentTimestamp: new Date(),
+	};
+
+	handleClick = () => {
+		this.setState((prevState) => {
+			return { clickCounter: prevState.clickCounter +1 };
+		});
+	};
+
+	componentDidMount() {
+		setInterval(() => {
+			this.setState({ currentTimestamp: new Date() })
+	}, 1000);
+	}
+
+ render() {
+    return (
+      <div>
+		<button onClick={this.handleClick}>Click</button>
+		<p>Clicked: {this.state.clickCounter}</p>
+		<p>Time: {this.state.currentTimestamp.toLocaleString()}</p>       
+      </div>
+    );
+  }
+}
+
+
+export default Test;
